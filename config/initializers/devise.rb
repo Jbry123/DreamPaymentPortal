ed7abfbba87920c9dcc37a8eccb760196dd4e300
Usman Ghani <usman.ghani720@gmail.com>
2021-09-04 23:35:14 +0500

    fixes

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,6 +9,7 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  config.secret_key = 'bb35c37df196db0006facf3854a940be817bfe76fd9628b357cd9e63d0013e4359b325550c78b94c501dd674cd5874ed79cc39b0cf581a00ab49e402ef91e00d'
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,16 @@
 const { environment } = require('@rails/webpacker')
+const jquery = require('./plugins/jquery')
 
-const webpack = require('webpack')
-environment.plugins.prepend('Provide',
+const webpack = require("webpack")
+
+environment.plugins.append('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
     jquery: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
+    jQuery: 'jquery/src/jquery',
+    Popper: ['popper.js', 'default']
   })
 )
 
+environment.plugins.prepend('jquery', jquery)
 module.exports = environment
